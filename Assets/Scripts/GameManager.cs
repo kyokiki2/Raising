3593deb8697474e387ee161443f52c36c4c8f669
kyo <kyokiki5@gmail.com>
2025-03-21 18:45:08 +0900

    working-2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public PlayerManager PlayerManager;
-    public ResourcePoolManager ResourcePoolManager;
+    public ObjectPoolManager ObjectPoolManager;
     public ChickenSpawnManager ChickenSpawnManager;
+    public DeliveryManManager DeliveryManManager;
+    public DeliveryStationManager DeliveryStationManager;
 
     public static GameManager Instance = null;
+
+    public Config Config { get { return config; } }
+    private Config config;
+
+    public DataAsset DataAsset { get { return dataAsset; } }
+    private DataAsset dataAsset;
 
     private void Awake()
     {
@@ -16,14 +24,26 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        Load();
         Init();
+    }
+
+    private void Load()
+    {
+        if (config == null)
+            config = Config.Load();
+
+        if (dataAsset == null)
+            dataAsset = DataAsset.Load();
     }
 
     private void Init()
     {
+        dataAsset.Init();
+        ObjectPoolManager.Init();
         PlayerManager.Init();
-        ResourcePoolManager.Init();
         ChickenSpawnManager.Init();
+        DeliveryManManager.Init();
     }
 
 
